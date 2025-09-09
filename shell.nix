f190aa52fd0cb0dedcd5f40e1dc0963500a83b72
Jeremy Shaw <jeremy@n-heptane.com>
2025-09-08 21:17:10 -0500

    0.23.12.4 - loosen build depends

--- shell.nix
+++ shell.nix
@@ -5,14 +5,14 @@ let
   inherit (nixpkgs) pkgs;
 
   f = { mkDerivation, base, bytestring, happstack-server, stdenv
-      , text, web-routes
+      , text, web-routes, cabal-install
       }:
       mkDerivation {
         pname = "web-routes-happstack";
         version = "0.23.10";
         src = ./.;
         libraryHaskellDepends = [
-          base bytestring happstack-server text web-routes
+          base bytestring happstack-server text cabal-install
         ];
         description = "Adds support for using web-routes with Happstack";
         license = stdenv.lib.licenses.bsd3;
